6358fbd6fe164acaee278f504382cb89ed6ae272
Corey Farrell <git@cfware.com>
2019-12-12 14:55:25 -0500

    Remove synonyms
    In addition to the assertion synonyms `Test#done` and `Test#tearDown`
    are removed.

--- test/tap/teardown-event-loop.js
+++ test/tap/teardown-event-loop.js
@@ -2,6 +2,6 @@ require('./')(
 t => {
   t.pass('fine')
   const interval = setInterval(() => {}, 10000)
-  t.tearDown(() => clearInterval(interval))
+  t.teardown(() => clearInterval(interval))
 }
 )
