c06fccc58e1c1baecfced201f2fc3bb532209a59
Corey Farrell <git@cfware.com>
2020-01-06 02:04:01 -0500

    Add `output` option to `libtap/settings`.
    This defaults to `process.stdout` but can be altered.  This is primarily
    to allow custom test runners to pipe test output directly to a reporter
    without additional processes.

---
+++ tap-snapshots/test-tap-redirect-output.js-TAP.test.js
@@ -0,0 +1,25 @@
+/* IMPORTANT
+ * This snapshot file is auto-generated, but designed for humans.
+ * It should be checked into source control and tracked carefully.
+ * Re-generate by setting TAP_SNAPSHOT=1 and running tests.
+ * Make sure to inspect the output below.  Do not ignore changes!
+ */
+'use strict'
+exports[`test/tap/redirect-output.js TAP > exit status 1`] = `
+Object {
+  "code": 0,
+  "signal": null,
+}
+`
+
+exports[`test/tap/redirect-output.js TAP > stderr 1`] = `
+
+`
+
+exports[`test/tap/redirect-output.js TAP > stdout 1`] = `
+redirected: TAP version 13
+redirected: ok 1 - ok
+redirected: 1..1
+redirected: # {time}
+
+`
